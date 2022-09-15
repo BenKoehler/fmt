08e2693251cd058e42ca5b532afd7cbb4b2b8fcb
BenKoehler <ben.koehler@web.de>
2022-09-15 19:53:57 +0200

    add compile definition BK_HAS_FMT

--- bk.cmake
+++ bk.cmake
@@ -1,8 +1,7 @@
 add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/fmt EXCLUDE_FROM_ALL)
 ConfigureCompileOptions(fmt-header-only INTERFACE)
-ConfigureCompileOptions(fmt PUBLIC)
+target_compile_definitions(fmt-header-only INTERFACE BK_HAS_FMT)
 
 function(link_fmt target MOD)
     target_link_libraries(${target} ${MOD} fmt-header-only)
-#    target_link_libraries(${target} ${MOD} fmt)
 endfunction()
