3a417eb3143a7ced281cbfa64adc08609f5bcf39
BenKoehler <ben.koehler@web.de>
2022-10-25 22:20:01 +0200

    add version macro

--- bk.cmake
+++ bk.cmake
@@ -1,3 +1,5 @@
+SET(BK_FMT_VERSION "9.1.0")
+
 add_subdirectory(${PROJECT_SOURCE_DIR}/thirdparty/fmt EXCLUDE_FROM_ALL)
 ConfigureCompileOptions(fmt-header-only INTERFACE)
 target_compile_definitions(fmt-header-only INTERFACE BK_HAS_FMT)
