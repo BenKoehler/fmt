3d09217af58795de3b8b8bde763f3e52aedde3ca
BenKoehler <ben.koehler@web.de>
2023-04-22 18:01:28 +0200

    remove BK_HAS_FMT macro definition

--- bk.cmake
+++ bk.cmake
@@ -2,7 +2,6 @@ SET(BK_FMT_VERSION "9.1.0")
 
 add_subdirectory(${PROJECT_SOURCE_DIR}/thirdparty/fmt EXCLUDE_FROM_ALL)
 ConfigureCompileOptions(fmt-header-only INTERFACE)
-target_compile_definitions(fmt-header-only INTERFACE BK_HAS_FMT)
 
 function(link_fmt target MOD)
     target_link_libraries(${target} ${MOD} fmt-header-only)
